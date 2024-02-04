7361b51bb8020dcdeeae090fe2488df1c37bb4f0
youcef khalfi jr <youcefkhalfi5@gmail.com>
2024-02-04 18:54:41 +0100

    add the project ssr

---
+++ Server-Rendering/webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    client: "./client/client.jsx",
+  },
+  output: {
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  /** can module be omitted for a simple project? No, it cannot. */
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+};
